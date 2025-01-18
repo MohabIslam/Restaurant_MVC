93ad19167524d6a2c55d9cb161e227f0ba99a0d8
Mohab_ELGhareeb@outlook.com <Mohab_ELGhareeb@outlook.com>
2025-01-18 10:14:10 +0200

    Add the Models that Needed [ ApplicationUser ,Category ,Ingredients ,Order ,OrderItem ,ProductIngredient ,Prouduct ]

---
+++ Restaurant/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using System.Security.Principal;
+
+namespace Restaurant.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string? UserId { get; set; } // This link for Navigationl by Id
+        public ApplicationUser User { get; set; } // 
+        public decimal TotalAmount { get; set; }
+        public ICollection<OrderItem> OrderItems{ get; set; }
+    }
+}
---
+++ Restaurant/Models/Prouduct.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Restaurant.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; } // Forgin Key Prop
+        public Category? MyProperty { get; set; } // A Product Belongs To A Catrgory , Navigationl Prop
+        public ICollection<OrderItem>? OrderItems { get; set; } // A Product Can Be in Many Order Items 
+        public ICollection<ProductIngredient>? ProductIngredients { get; set; } // A Product Can Habe Many Ingredient
+    }
+}
